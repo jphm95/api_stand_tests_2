ac487623d2fc1afa1774e1b9ccd6020644a677fc
jphm95 <163238322+jphm95@users.noreply.github.com>
2024-03-17 12:43:24 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,5 @@
+URL_SERVICE = "https://cnt-85797740-e99e-4f50-8086-d5a801532edc.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
---
+++ sender_stand_request.py
@@ -0,0 +1,39 @@
+import configuration
+import requests
+import data
+
+
+def get_docs():
+    return requests.get(configuration.URL_SERVICE + configuration.DOC_PATH)
+
+
+response = get_docs()
+print(response.status_code)
+
+
+def get_logs():
+    return requests.get(configuration.URL_SERVICE + configuration.LOG_MAIN_PATH)
+
+
+response = get_logs()
+print(response.status_code)
+print(response.headers)
+
+
+def get_users_table():
+    return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH)
+
+
+response = get_users_table()
+print(response.status_code)
+
+
+def post_new_user(body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
+                         json=body,
+                         headers=data.headers)
+
+
+response = post_new_user(data.user_body)
+print(response.status_code)
+print(response.json())
